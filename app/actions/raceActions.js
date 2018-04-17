ee6f535cf7d65a6692df03dbcd1b0137ab330207
Emilien Perico <emilien.perico@gmail.com>
2018-04-17 12:16:24 +1000

    update endpoint url to call next 5 server

--- app/actions/raceActions.js
+++ app/actions/raceActions.js
@@ -10,7 +10,7 @@ const NB_RACES = 5
 * Fetches specified number of non finished races from TAB API.
 */
 const fetchRaces = () => {
-  const url = `${FETCH_RACES_URL}&maxRaces=${NB_RACES}`;
+  const url = `${FETCH_RACES_URL}?maxRaces=${NB_RACES}`;
   return {
     type: types.FETCH_RACES,
     payload: axios.get(url)
--- app/constants/Endpoints.js
+++ app/constants/Endpoints.js
@@ -1,2 +1,5 @@
-// query string excludes recently closed races
-export const FETCH_RACES_URL = "https://api.beta.tab.com.au/v1/tab-info-service/racing/next-to-go/races?jurisdiction=NSW&includeRecentlyClosed=false"
+// Call Tab API
+// export const FETCH_RACES_URL = "https://api.beta.tab.com.au/v1/tab-info-service/racing/next-to-go/races?jurisdiction=NSW&includeRecentlyClosed=false"
+
+// Call Next 5 server to fetch the races
+export const FETCH_RACES_URL = "http://localhost:3000/races"
